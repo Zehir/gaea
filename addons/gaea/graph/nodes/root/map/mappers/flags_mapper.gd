96706a544244755e87f4b60c57706d21fe0c75ec
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-03-23 12:55:21 -0300

    Make mapper functionality abstract

--- addons/gaea/graph/nodes/root/map/mappers/flags_mapper.gd
+++ addons/gaea/graph/nodes/root/map/mappers/flags_mapper.gd
@@ -1,44 +1,17 @@
 @tool
-extends GaeaNodeResource
+extends "res://addons/gaea/graph/nodes/root/map/mappers/basic_mapper.gd"
 
 
-func get_data(output_port: int, area: AABB, generator_data: GaeaData) -> Dictionary[Vector3i, GaeaMaterial]:
-	if get_connected_resource_idx(0) == -1:
-		return {}
-
-	var data_input_resource: GaeaNodeResource = generator_data.resources.get(get_connected_resource_idx(0))
-	var passed_data: Dictionary = data_input_resource.get_data(
-		get_connected_port_to(0),
-		area, generator_data
-	)
-	var material: GaeaMaterial = null
-
-	if get_connected_resource_idx(1) != -1:
-		var material_input_resource: GaeaNodeResource = generator_data.resources[get_connected_resource_idx(1)]
-		if is_instance_valid(material_input_resource):
-			material = material_input_resource.get_data(
-				get_connected_port_to(1),
-				area, generator_data
-			).value
-	var grid: Dictionary[Vector3i, GaeaMaterial]
+func _passes_mapping(passed_data: Dictionary, cell: Vector3i, generator_data: GaeaData) -> bool:
 	var flags: Array = get_arg("match_flags", generator_data)
 	var exclude_flags: Array = get_arg("exclude_flags", generator_data)
 	var match_all: bool = get_arg("match_all", generator_data)
 
-	for cell in passed_data:
-		var value: float = passed_data[cell]
-		if (
-			(flags.all(_matches_flag.bind(value)) if match_all else flags.any(_matches_flag.bind(value)))
-			 and not exclude_flags.any(_matches_flag.bind(value))
-			):
-			if is_instance_valid(material):
-				grid[cell] = material.get_resource()
-			else:
-				grid[cell] = null
-		else:
-			grid[cell] = null
-
-	return grid
+	var value: float = passed_data[cell]
+	if match_all:
+		return flags.all(_matches_flag.bind(value)) and not exclude_flags.any(_matches_flag.bind(value))
+	else:
+		return flags.any(_matches_flag.bind(value)) and not exclude_flags.any(_matches_flag.bind(value))
 
 
 func _matches_flag(value: float, flag: int) -> bool:
--- addons/gaea/graph/nodes/root/map/mappers/value_mapper.gd
+++ addons/gaea/graph/nodes/root/map/mappers/value_mapper.gd
@@ -3,6 +3,5 @@ extends "res://addons/gaea/graph/nodes/root/map/mappers/basic_mapper.gd"
 
 
 func _passes_mapping(passed_data: Dictionary, cell: Vector3i, generator_data: GaeaData) -> bool:
-	var range: Dictionary = get_arg("range", generator_data)
-	var value = passed_data.get(cell)
-	return value <= range.get("min", 0.0) or value >= range.get("max", 0.0)
+	var value: float = get_arg("value", generator_data)
+	return passed_data.get(cell) == value
