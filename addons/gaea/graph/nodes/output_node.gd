d86985713a440ec73cf9cc5995550d0dcbbfdfc1
Zehir <zehir@zorim.fr>
2025-03-31 18:44:58 +0200

    Added Layer resource name and state on output node.

--- addons/gaea/graph/nodes/output_node.gd
+++ addons/gaea/graph/nodes/output_node.gd
@@ -19,6 +19,9 @@ func _add_layer_slot(idx: int) -> void:
 	slot_resource.left_type = GaeaGraphNode.SlotTypes.MAP_DATA
 	slot_resource.right_enabled = false
 	add_child(slot_resource.get_node())
+	_connect_layer_resource_signal(idx)
+	await get_tree().process_frame
+	size.y = get_combined_minimum_size().y
 
 
 func update_slots() -> void:
@@ -32,3 +35,30 @@ func update_slots() -> void:
 	elif layer_count > get_child_count():
 		for i in range(get_child_count(), layer_count):
 			_add_layer_slot(i)
+	
+	for idx in layer_count:
+		_connect_layer_resource_signal(idx)
+
+
+func _connect_layer_resource_signal(idx: int):
+	var layer: GaeaLayer = generator.data.layers[idx]
+	if not layer or not is_instance_valid(layer):
+		return
+	if layer.changed.is_connected(_on_layer_resource_changed):
+		return
+
+	var node: Node = get_child(idx - 1)
+	var callback = _on_layer_resource_changed.bind(idx, layer)
+	layer.changed.connect(callback)
+	node.tree_exiting.connect(layer.changed.disconnect.bind(callback), CONNECT_ONE_SHOT)
+	callback.call_deferred()
+
+func _on_layer_resource_changed(idx: int, layer: GaeaLayer):
+	var slot: Node = get_child(idx)
+	if layer.resource_name:
+		slot.left_label = layer.resource_name
+	else:
+		slot.left_label = "Layer %s" % idx
+	
+	if not layer.enabled:
+		slot.left_label = "[color=DIM_GRAY]%s[/color] (Disabled)" % slot.left_label
