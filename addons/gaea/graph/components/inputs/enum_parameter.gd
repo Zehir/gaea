d164a6d3feb23c616fa4b754b124b5bae0d43f52
Zehir <zehir@zorim.fr>
2025-04-16 11:41:13 +0200

    Add float op

--- addons/gaea/graph/components/inputs/enum_parameter.gd
+++ addons/gaea/graph/components/inputs/enum_parameter.gd
@@ -11,3 +11,32 @@ func _ready() -> void:
 
 	if not is_instance_valid(resource):
 		return
+
+
+	var options_key = resource.name.capitalize()
+	var options = graph_node.resource.get(options_key)
+	if typeof(options) != TYPE_DICTIONARY:
+		var script = graph_node.resource.get_script()
+		if script is Script:
+			push_error("Could not find Enum named '%s' in script %s." % [options_key , script.get_path()])
+		else:
+			push_error("Could not find Enum named '%s' in node resource script." % options_key)
+		return
+	
+	for option in options:
+		option_button.add_item(option, options.get(option))
+
+	option_button.item_selected.connect(param_value_changed.emit)
+
+
+func get_param_value() -> int:
+	if super() != null:
+		return super()
+	return option_button.get_selected_id()
+
+
+func set_param_value(new_value: Variant) -> void:
+	print("set_param_value", new_value)
+	if typeof(new_value) != TYPE_INT:
+		return
+	option_button.select(new_value)
--- addons/gaea/graph/nodes/root/scalar/operations/float_op.gd
+++ addons/gaea/graph/nodes/root/scalar/operations/float_op.gd
@@ -1,2 +1,47 @@
 @tool
 extends GaeaNodeResource
+
+enum Operation {
+	Add,
+	Substract,
+	Multiply,
+	Divide,
+	Remainder,
+	Power,
+	Max,
+	Min,
+	ATan2,
+	Snapped,
+}
+
+
+func get_data(_passed_data:Array[Dictionary], output_port: int, _area: AABB, generator_data: GaeaData) -> Dictionary:
+	log_data(output_port, generator_data)
+
+	var a: float = get_arg("a", generator_data)
+	var b: float = get_arg("b", generator_data)
+	var operation: Operation = get_arg("operation", generator_data)
+
+	return {"value": _get_new_value(a, b, operation)}
+
+
+func _get_new_value(a: float, b: float, operation: Operation) -> float:
+	if typeof(a) != typeof(b):
+		return a
+
+	match operation:
+		Operation.Add: return a + b
+		Operation.Substract: return a - b
+		Operation.Multiply: return a * b
+		Operation.Divide: return 0.0 if is_zero_approx(b) else a / b
+		Operation.Remainder: return 0.0 if is_zero_approx(b) else fmod(a, b)
+		Operation.Power: return pow(a, b)
+		Operation.Max: return maxf(a, b)
+		Operation.Min: return minf(a, b)
+		Operation.ATan2: return atan2(a, b)
+		Operation.Snapped: return snappedf(a, b)
+	return a
+
+
+func _get_required_input_ports() -> Array[int]:
+	return [0, 1]
