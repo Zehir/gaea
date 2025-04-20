2e9068f69259bd2786de5ff49d5141e012c5a75f
Zehir <zehir@zorim.fr>
2025-04-20 17:51:07 +0200

    Update position conversion methods

--- addons/gaea/nodes/renderers/tilemap_renderer.gd
+++ addons/gaea/nodes/renderers/tilemap_renderer.gd
@@ -9,120 +9,105 @@ extends GaeaRenderer
 @export var tile_map_layers: Array[TileMapLayer] = []
 
 
-static var position_conversion: Dictionary[TileSet.TileOffsetAxis, Dictionary] = {
-	TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_HORIZONTAL: {
-		TileSet.TileLayout.TILE_LAYOUT_STACKED: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
+# Conversion methods adapted from https://github.com/Zehir/godot-hexagon-tile-map-layer
+## This method return a [Callable] to convert the Vector3i position from the Map object to the TileMapLayer map position.
+static func _get_position_conversion_method(tile_set: TileSet) -> Callable:
+	match tile_set.tile_shape:
+		TileSet.TileShape.TILE_SHAPE_SQUARE, \
+		TileSet.TileShape.TILE_SHAPE_HEXAGON, \
+		TileSet.TileShape.TILE_SHAPE_HALF_OFFSET_SQUARE:
 
-			var l_x = cube_position.x + ((cube_position.y & ~1) >> 1)
-			var l_y = cube_position.y
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_STACKED_OFFSET: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
+			match tile_set.tile_offset_axis:
+				TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_HORIZONTAL:
+					match tile_set.tile_layout:
+						TileSet.TileLayout.TILE_LAYOUT_STACKED: return (func(pos: Vector3i) -> Vector2i:
+							return Vector2i(pos.x, pos.y)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_STACKED_OFFSET: return (func(pos: Vector3i) -> Vector2i:
+							return Vector2i(pos.x + (pos.y & 1), pos.y)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_STAIRS_RIGHT: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.x - ((pos.y & ~1) >> 1)
+							return Vector2i(part, pos.y)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_STAIRS_DOWN: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.x - ((pos.y & ~1) >> 1)
+							return Vector2i(part + part + pos.y, -part)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_DIAMOND_RIGHT: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.x - ((pos.y & ~1) >> 1)
+							return Vector2i(part, part + pos.y)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_DIAMOND_DOWN: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.x - ((pos.y & ~1) >> 1)
+							return Vector2i(part + pos.y, -part)
+						)
 
-			var x = cube_position.x + (cube_position.y + 1 >> 1)
-			var y = cube_position.y
-			return Vector2i(x, y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_STAIRS_RIGHT: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
+				TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_VERTICAL:
+					match tile_set.tile_layout:
+						TileSet.TileLayout.TILE_LAYOUT_STACKED: return (func(pos: Vector3i) -> Vector2i:
+							return Vector2i(pos.x, pos.y)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_STACKED_OFFSET: return (func(pos: Vector3i) -> Vector2i:
+							return Vector2i(pos.x, pos.y + (pos.x & 1))
+						)
+						TileSet.TileLayout.TILE_LAYOUT_STAIRS_RIGHT: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.y - ((pos.x - (pos.x & 1)) >> 1)
+							return Vector2i(-part, pos.x + part + part)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_STAIRS_DOWN: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.y - ((pos.x - (pos.x & 1)) >> 1)
+							return Vector2i(pos.x, part)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_DIAMOND_RIGHT: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.y - ((pos.x - (pos.x & 1)) >> 1)
+							return Vector2i(-part, pos.x + part)
+						)
+						TileSet.TileLayout.TILE_LAYOUT_DIAMOND_DOWN: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.y - ((pos.x - (pos.x & 1)) >> 1)
+							return Vector2i(pos.x + part, part)
+						)
 
-			var l_x = cube_position.x
-			var l_y = cube_position.y
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_STAIRS_DOWN: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
+		TileSet.TileShape.TILE_SHAPE_ISOMETRIC:
+			# Only the 2 stacked layout need axis dependant methods
+			match tile_set.tile_layout:
+				TileSet.TileLayout.TILE_LAYOUT_STACKED:
+					match tile_set.tile_offset_axis:
+						TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_HORIZONTAL: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.y - pos.x
+							return Vector2i(pos.x + (part >> 1), part)
+						)
+						TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_VERTICAL: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.x + pos.y
+							return Vector2i(part, -pos.x + (part >> 1))
+						)
+				TileSet.TileLayout.TILE_LAYOUT_STACKED_OFFSET:
+					match tile_set.tile_offset_axis:
+						TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_HORIZONTAL: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.y - pos.x
+							return Vector2i(pos.x + (part + 1 >> 1), part)
+						)
+						TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_VERTICAL: return (func(pos: Vector3i) -> Vector2i:
+							var part = pos.x + pos.y
+							return Vector2i(part, -pos.x + (part + 1 >> 1))
+						)
+				TileSet.TileLayout.TILE_LAYOUT_STAIRS_RIGHT: return (func(pos: Vector3i) -> Vector2i:
+					return Vector2i(pos.x, pos.y - pos.x)
+				)
+				TileSet.TileLayout.TILE_LAYOUT_STAIRS_DOWN: return (func(pos: Vector3i) -> Vector2i:
+					return Vector2i(pos.y + pos.x, -pos.x)
+				)
+				TileSet.TileLayout.TILE_LAYOUT_DIAMOND_RIGHT: return (func(pos: Vector3i) -> Vector2i:
+					return Vector2i(pos.x, pos.y)
+				)
+				TileSet.TileLayout.TILE_LAYOUT_DIAMOND_DOWN: return (func(pos: Vector3i) -> Vector2i:
+					return Vector2i(pos.y, -pos.x)
+				)
 
-			var l_y = -cube_position.x
-			var l_x = -cube_position.z - l_y
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_DIAMOND_RIGHT: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
+	push_error("No conversion method for this tile_set")
+	return Callable()
 
-			var l_x = cube_position.x
-			var l_y = -cube_position.z
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_DIAMOND_DOWN: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
 
-			var l_x = -cube_position.z
-			var l_y = -cube_position.x
-			return Vector2i(l_x, l_y)
-			),
-	},
-		TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_VERTICAL: {
-		TileSet.TileLayout.TILE_LAYOUT_STACKED: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
-
-			var l_x = cube_position.x
-			var l_y = cube_position.y + ((cube_position.x - (cube_position.x & 1)) >> 1)
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_STACKED_OFFSET: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
-
-			var l_x = cube_position.x
-			var l_y = cube_position.y + ((cube_position.x + (cube_position.x & 1)) >> 1)
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_STAIRS_RIGHT: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
-
-			var l_x = -cube_position.y
-			var l_y = cube_position.x + 2 * cube_position.y
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_STAIRS_DOWN: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
-
-			var l_x = cube_position.x
-			var l_y = cube_position.y
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_DIAMOND_RIGHT: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
-
-			var l_x = -cube_position.y
-			var l_y = -cube_position.z
-			return Vector2i(l_x, l_y)
-			),
-		TileSet.TileLayout.TILE_LAYOUT_DIAMOND_DOWN: (func(pos: Vector3i) -> Vector2i:
-			var cube_x = pos.x - ((pos.y & ~1) >> 1)
-			var cube_y = pos.y
-			var cube_position = Vector3i(cube_x, cube_y, -cube_x - cube_y)
-
-			var l_x = -cube_position.z
-			var l_y = cube_position.y
-			return Vector2i(l_x, l_y)
-			),
-	}
-}
 
 func _render(grid: GaeaGrid) -> void:
 	_reset()
@@ -132,16 +117,28 @@ func _render(grid: GaeaGrid) -> void:
 	for layer_idx in grid.get_layers_count():
 		if tile_map_layers.size() <= layer_idx or not is_instance_valid(tile_map_layers.get(layer_idx)):
 			continue
-		for cell in grid.get_layer(layer_idx):
-			var value = grid.get_layer(layer_idx)[cell]
+		
+		var tile_map_layer = tile_map_layers[layer_idx]
+		var tile_set = tile_map_layer.tile_set
+		if not is_instance_valid(tile_set):
+			push_error("Can't render layer %d without a TileSet" % layer_idx)
+			continue
+
+		var position_conversion: Callable = _get_position_conversion_method(tile_set)
+		
+		var cells = grid.get_layer(layer_idx)
+		for cell in cells:
+			var value = cells[cell]
 			if value is TileMapMaterial:
 				if value.type == TileMapMaterial.Type.SINGLE_CELL:
-					tile_map_layers[layer_idx].set_cell(Vector2i(cell.x, cell.y), value.source_id, value.atlas_coord, value.alternative_tile)
+					
+					tile_map_layer.set_cell(position_conversion.call(cell), value.source_id, value.atlas_coord, value.alternative_tile)
+					#tile_map_layer.set_cell(Vector2i(cell.x, cell.y), value.source_id, value.atlas_coord, value.alternative_tile)
 				elif value.type == TileMapMaterial.Type.TERRAIN:
-					terrains.get_or_add(value, []).append(Vector2i(cell.x, cell.y))
+					terrains.get_or_add(value, []).append(position_conversion.call(cell))
 
 		for material: TileMapMaterial in terrains:
-			tile_map_layers[layer_idx].set_cells_terrain_connect(
+			tile_map_layer.set_cells_terrain_connect(
 				terrains.get(material), material.terrain_set, material.terrain
 			)
 
