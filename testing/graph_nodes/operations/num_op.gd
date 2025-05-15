ddae68cf3deda78959142cf5a6c9de93bd0e5e79
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-05-15 06:38:44 -0300

    Add Operation node checks and fix `GaeaNodeDataOp` (#402)
    * Add float op test script
    
    * Add FloatOp and IntOp checks
    
    * Add `GaeaNodeDataOp` test
    
    * Oops
    
    * Add `GaeaNodeDatasOp` test

---
+++ testing/graph_nodes/operations/num_op.gd
@@ -0,0 +1,22 @@
+extends GdUnitTestSuite
+
+
+var node: GaeaNodeNumOp
+
+
+func before():
+	pass
+
+
+func _assert_operation_result(args: Array[Variant], expected: float) -> void:
+	for i in node.get_arguments_list().size():
+		node.set_argument_value(node.get_arguments_list()[i], args[i])
+	var result: float = node._get_data(&"result", AABB(), null)
+	assert_that(result)\
+		.override_failure_message(_get_failure_message())\
+		.append_failure_message("Arguments: %s\n Expected result: %s\n Returned result: %s" % [args, expected, result])\
+		.is_equal(expected)
+
+
+func _get_failure_message() -> String:
+	return ""
