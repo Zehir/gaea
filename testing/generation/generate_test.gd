85efd5890e05a13a59948f2740e8af1fd00591aa
Ian Cullum <cullumi@wwu.edu>
2025-11-05 19:49:54 -0800

    Move demos to a submodule (2.0) (#425)
    * Removed scenes folder
    * gaea-demos submodule added.
    * Copied walker demo over to testing folder so that the generation test isn't dependent on a submodule.
    * Changes name of the generate test's floor_walker GaeaData to generation_walker.
    * Replaced base_walker.tres with generation_walker.tres, gaea_graph.gd now uses generate_test.tscn instead of walker_demo.tscn.
    * Gave class_names to renderer_test_scene.gd and generation_tester.gd
    * Updated demos repo to save_version 5
    * Cleared up a bunch of warnings
    * Added more specifically typed renderer properties to gridmap_renderer_test.gd and tilemap_renderer_test.gd for better type checking
    * Changed "seed" to "generation_seed" in generate_test.gd

--- testing/generation/generate_test.gd
+++ testing/generation/generate_test.gd
@@ -2,28 +2,32 @@ extends GdUnitTestSuite
 
 
 var first_grid: GaeaGrid
+var generator_seed:int
 
-
+const test_scene = "uid://dh5c2eomfri6n"
 
 func test_has_generated() -> void:
-	var scene = load("uid://di7u4f3idjdd").instantiate()
+	var scene : GaeaGenerationTester = load(test_scene).instantiate()
 	var _runner := scene_runner(scene)
 	await scene.test_generation()
 	first_grid = scene.last_grid
+	generator_seed = scene.gaea_generator.seed
 	assert_dict(first_grid._grid).is_not_empty()
 
 
 func test_generations_match() -> void:
-	var scene = load("uid://di7u4f3idjdd").instantiate()
+	var scene : GaeaGenerationTester = load(test_scene).instantiate()
 	var _runner := scene_runner(scene)
 	await scene.test_generation()
 	var second_grid: GaeaGrid = scene.last_grid
+	assert_that(generator_seed).is_equal(scene.gaea_generator.seed)
 	assert_bool(first_grid._grid.recursive_equal(second_grid._grid, 1)).is_true()
 
 
 func test_generations_dont_match() -> void:
-	var scene = load("uid://di7u4f3idjdd").instantiate()
+	var scene : GaeaGenerationTester = load(test_scene).instantiate()
 	var _runner := scene_runner(scene)
-	await scene.test_generation(1)
+	await scene.test_generation(5)
 	var second_grid: GaeaGrid = scene.last_grid
+	assert_that(generator_seed).is_not_equal(scene.gaea_generator.seed)
 	assert_bool(first_grid._grid.recursive_equal(second_grid._grid, 1)).is_false()
