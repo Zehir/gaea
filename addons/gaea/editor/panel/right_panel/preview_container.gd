c86fbdf0ff8dd2bd2239314516e413e3319511e7
Zehir <zehir@zorim.fr>
2025-11-14 00:51:10 +0100

    Add multi mesh

--- addons/gaea/editor/panel/right_panel/preview_container.gd
+++ addons/gaea/editor/panel/right_panel/preview_container.gd
@@ -3,9 +3,8 @@ extends SubViewportContainer
 
 @export var target_world_3d: World3D
 @export var view_port: SubViewport
-@export var object_3d: Node3D
 @export var camera: Camera3D
-
+@export var multi_mesh_instance: MultiMeshInstance3D
 
 func _ready() -> void:
 	if is_part_of_edited_scene():
@@ -13,6 +12,9 @@ func _ready() -> void:
 
 	view_port.world_3d = target_world_3d
 
+	multi_mesh_instance.multimesh.instance_count = 1
+	multi_mesh_instance.multimesh.set_instance_transform(0, Transform3D(Basis(), Vector3(0.0, 0.0, 0.0)))
+	multi_mesh_instance.multimesh.set_instance_color(0, Color.BLUE)
 	pass
 	#await resized
 	#get_parent().split_offset = int(size.x)
