b845c4252147f0e542fb0b78ce555fc89daaac42
Zehir <zehir@zorim.fr>
2025-11-13 21:27:04 +0100

    Preview panel changes from first branch

---
+++ addons/gaea/editor/panel/right_panel/preview_container.gd
@@ -0,0 +1,15 @@
+@tool
+extends SubViewportContainer
+
+@export var target_world_3d: World3D
+@export var view_port: SubViewport
+
+func _init() -> void:
+	pass
+	#if not is_part_of_edited_scene():
+	#	view_port.world_3d = target_world_3d
+
+
+func _ready() -> void:
+	await resized
+	get_parent().split_offset = int(size.x)
---
+++ addons/gaea/editor/panel/right_panel/right_panel.gd
@@ -0,0 +1,46 @@
+@tool
+class_name GaeaPreviewPanel
+extends Control
+
+@export var main_editor: GaeaMainEditor
+
+@onready var generation_settings_container: VBoxContainer = %GenerationSettingsContainer
+
+var _settings_inspector: EditorInspector
+
+
+func _ready() -> void:
+	if is_part_of_edited_scene() or not is_instance_valid(main_editor):
+		return
+	_build_inspector()
+
+
+func _on_button_pressed() -> void:
+	pass
+
+
+
+func _build_inspector() -> void:
+	_settings_inspector = EditorInspector.new()
+	_settings_inspector.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	_settings_inspector.edit(null)
+	_settings_inspector.edit(main_editor.settings)
+	_settings_inspector.property_edited.connect(_on_setting_updated)
+	generation_settings_container.add_child(_settings_inspector)
+
+	# Workaround until this issue is resolved : https://github.com/godotengine/godot/issues/112647
+	await get_tree().create_timer(0.5).timeout
+	for editor_property_vector3 in _settings_inspector.find_children("*", "EditorPropertyVector3", true, false):
+		for node in editor_property_vector3.find_children("*", "EditorSpinSlider", true, false):
+			if node is EditorSpinSlider:
+				node.allow_lesser = false
+				node.allow_greater = false
+
+
+func _on_setting_updated(property_name: StringName):
+	if property_name == &"world_size_preset" and main_editor.settings.world_size_preset != GaeaPreviewGenerationSettings.WorldSizePreset.CUSTOM:
+		main_editor.settings.world_size = main_editor.settings.property_get_revert(&"world_size")
+		main_editor.settings.cell_size = main_editor.settings.property_get_revert(&"cell_size")
+	if property_name == &"world_size" or property_name == &"cell_size":
+		main_editor.settings.world_size_preset = GaeaPreviewGenerationSettings.WorldSizePreset.CUSTOM
+	main_editor.generation_settings_changed.emit()
