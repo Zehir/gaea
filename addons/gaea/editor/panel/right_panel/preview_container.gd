5ccd05dd35ce5938a0e8294225bf8d5835646a7f
Zehir <zehir@zorim.fr>
2025-11-14 00:25:30 +0100

    Add orbit camera

--- addons/gaea/editor/panel/right_panel/preview_container.gd
+++ addons/gaea/editor/panel/right_panel/preview_container.gd
@@ -3,13 +3,20 @@ extends SubViewportContainer
 
 @export var target_world_3d: World3D
 @export var view_port: SubViewport
-
-func _init() -> void:
-	pass
-	#if not is_part_of_edited_scene():
-	#	view_port.world_3d = target_world_3d
+@export var object_3d: Node3D
+@export var camera: Camera3D
 
 
 func _ready() -> void:
-	await resized
-	get_parent().split_offset = int(size.x)
+	if is_part_of_edited_scene():
+		return
+
+	view_port.world_3d = target_world_3d
+
+	pass
+	#await resized
+	#get_parent().split_offset = int(size.x)
+
+
+func _gui_input(event: InputEvent) -> void:
+	camera.input(event)
