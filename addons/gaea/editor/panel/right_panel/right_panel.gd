9addbc7d02acdef9937bfcf04aa241fdb87286e6
Zehir <zehir@zorim.fr>
2025-11-24 21:51:02 +0100

    It's working !

--- addons/gaea/editor/panel/right_panel/right_panel.gd
+++ addons/gaea/editor/panel/right_panel/right_panel.gd
@@ -6,29 +6,35 @@ extends Control
 @export var preview_container: GaeaPreviewContainer
 @export var bottom_label: Label
 @export var bottom_container: VBoxContainer
+@export var generate_button: Button
 
 var generation_in_progress: bool = false
 
 
-func _on_output_node_traversed(_port: StringName, data: Variant, pouch: GaeaGenerationPouch) -> void:
-	if data is GaeaGrid:
-		#prints("_on_output_node_traversed", pouch.area.position, data.get_layer(0).get_cell_count())
-		preview_container.draw_grid(data, pouch.area.position)
-
-
-func unused_to_be_removed_on_button_pressed() -> void:
+func _on_generate_button_pressed() -> void:
 	if generation_in_progress:
 		return
+	generate_button.disabled = true
 	generation_in_progress = true
 
-	#var start = Time.get_ticks_usec()
-	#var generated_region = AABB(Vector3.ZERO, settings.cell_size)
-	#var pouch: GaeaGenerationPouch = main_editor.graph_edit.get_pouch(generated_region)
-	#var graph: GaeaGraph = main_editor.graph_edit.graph
-	#var data: GaeaGrid = graph.get_output_node().execute(graph, pouch)
-	#var generation_duration = (Time.get_ticks_usec() - start) * 0.001
-	#start = Time.get_ticks_usec()
-	#preview_container.draw_grid(data, settings.cell_size * -0.5)
-	#generation_in_progress = false
-	#var render = (Time.get_ticks_usec() - start) * 0.001
-	#bottom_label.text = "Generated in %d ms, render in %d ms" % [generation_duration, render]
+	var start = Time.get_ticks_usec()
+	var graph: GaeaGraph = main_editor.graph_edit.graph
+	var area = AABB(Vector3.ZERO, graph.preview_chunk_size)
+	var settings: GaeaGenerationSettings = GaeaGenerationSettings.new()
+	settings.cell_size = graph.preview_chunk_size
+	settings.world_size = graph.preview_chunk_size
+	settings.random_seed_on_generate = false
+	settings.seed = graph.preview_seed
+
+	var pouch: GaeaGenerationPouch = GaeaGenerationPouch.new(settings, area)
+	var data: GaeaGrid = graph.get_output_node().execute(graph, pouch)
+	var generation_duration = (Time.get_ticks_usec() - start) * 0.001
+
+	preview_container.draw_grid(data, settings.cell_size * -0.5)
+
+	var render = (Time.get_ticks_usec() - start) * 0.001
+	bottom_label.text = "Generated in %d ms, render in %d ms" % [generation_duration, render]
+
+
+	generation_in_progress = false
+	generate_button.disabled = false
