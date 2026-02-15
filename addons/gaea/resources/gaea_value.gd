59c78ae12fc70b89447e4cc6ad9c5a00ae8afd19
Zehir <zehir@zorim.fr>
2026-02-15 22:23:46 +0100

    Escape GaeaEditorSettings calls during runtime (#577)
    * Escape GaeaEditorSettings calls during runtime

--- addons/gaea/resources/gaea_value.gd
+++ addons/gaea/resources/gaea_value.gd
@@ -69,13 +69,6 @@ static func get_type_string(type: Type) -> String:
 	return String(Type.find_key(type)).capitalize().replace(" ", "")
 
 
-## Returns the configured color for slots of [param type].
-static func get_color(type: Type) -> Color:
-	if GaeaEditorSettings.CONFIGURABLE_SLOT_COLORS.has(type):
-		return GaeaEditorSettings.get_configured_color_for_value_type(type)
-	return get_default_color(type)
-
-
 ## Returns the default value for [param type]. Returns [code]null[/code] if there's none.
 static func get_default_value(type: Type) -> Variant:
 	match type:
@@ -157,6 +150,16 @@ static func from_old_slot_type(old_type: int) -> GaeaValue.Type:
 	return GaeaValue.Type.NULL
 
 
+## Returns the configured color for slots of [param type].
+static func get_color(type: Type) -> Color:
+	if Engine.is_editor_hint():
+		# gdlint:ignore = duplicated-load
+		var gaea_editor_settings: GDScript = load("uid://duu3vekk7pxwk")
+		if gaea_editor_settings.CONFIGURABLE_SLOT_COLORS.has(type):
+			return gaea_editor_settings.get_configured_color_for_value_type(type)
+	return get_default_color(type)
+
+
 ## Returns the default color for slots of [param type].
 static func get_default_color(type: Type) -> Color:
 	match type:
@@ -210,8 +213,8 @@ static func get_display_icon(type: Type) -> Texture2D:
 		Type.MATERIAL:
 			return load("uid://b0vqox8bodse")
 		Type.TEXTURE:
-			var editor_interface = Engine.get_singleton("EditorInterface")
-			return editor_interface.get_base_control().get_theme_icon(&"Image", &"EditorIcons")
+			if Engine.is_editor_hint():
+				return Engine.get_singleton(&"EditorInterface").get_base_control().get_theme_icon(&"Image", &"EditorIcons")
 		# Dictionary types
 		Type.SAMPLE:
 			return load("uid://dkccxw7yq1mth")
@@ -222,8 +225,11 @@ static func get_display_icon(type: Type) -> Texture2D:
 
 ## Returns the configured icon for slots of [param type].
 static func get_slot_icon(type: Type) -> Texture2D:
-	if GaeaEditorSettings.CONFIGURABLE_SLOT_COLORS.has(type):
-		return GaeaEditorSettings.get_configured_icon_for_value_type(type)
+	if Engine.is_editor_hint():
+		# gdlint:ignore = duplicated-load
+		var gaea_editor_settings: GDScript = load("uid://duu3vekk7pxwk")
+		if gaea_editor_settings.CONFIGURABLE_SLOT_COLORS.has(type):
+			return gaea_editor_settings.get_configured_icon_for_value_type(type)
 	return get_default_slot_icon(type)
 
 
@@ -240,7 +246,8 @@ static func get_default_slot_icon(type: Type) -> Texture2D:
 		Type.VECTOR3I, Type.VECTOR3:
 			return load("uid://dbvw3j8fnmhpu")
 		Type.ANY:
-			return EditorInterface.get_editor_theme().get_icon("NodeInfo", "EditorIcons")
+			if Engine.is_editor_hint():
+				return Engine.get_singleton(&"EditorInterface").get_editor_theme().get_icon("NodeInfo", "EditorIcons")
 		# Simple types
 		Type.RANGE:
 			return load("uid://dfsmxavxasx7x")
